785b4fee13ad696a011fc2d2e688a821fb36648c
fidelia <fidelialtm13@gmail.com>
2024-03-06 20:20:46 +0200

    Android Login Page

--- MyLogin/app/src/main/java/com/example/mylogin/LoginScreen.kt
+++ MyLogin/app/src/main/java/com/example/mylogin/LoginScreen.kt
@@ -1,11 +1,16 @@
 package com.example.mylogin
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -14,16 +19,27 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(){
+
+   var email by remember {
+      mutableStateOf("")
+   }
+
+   var password by remember {
+      mutableStateOf("")
+   }
 
    Column(
       modifier = Modifier.fillMaxSize(),
@@ -38,25 +54,57 @@ fun LoginScreen(){
       
       Text(text = "Login to your account")
 
-      OutlinedTextField(value = "", onValueChange ={}, label = { Text(text = "Email address")} )
+      OutlinedTextField(value = email, onValueChange ={email=it}, label = { Text(text = "Email address")} )
 
       Spacer(modifier = Modifier.height(16.dp))
 
-      OutlinedTextField(value = "", onValueChange ={}, label = { Text(text = "Password")} )
+      OutlinedTextField(value = password, onValueChange ={password=it}, label = { Text(text = "Password")},
+         visualTransformation = PasswordVisualTransformation())
 
       Spacer(modifier = Modifier.height(16.dp))
       
-      Button(onClick = { }) {
+      Button(onClick = { Log.i("Credential", "Email :$email Password: $password")}) {
          Text(text = "Login")
          
       }
       
-      Spacer(modifier = Modifier.height(32.dp))
+      Spacer(modifier = Modifier.height(25.dp))
 
       TextButton(onClick = { }) {
          Text(text = "Forgot Password?")
 
       }
+      
+      Spacer(modifier = Modifier.height(25.dp))
+      
+      Text(text = "Or sign in with", modifier = Modifier.clickable {  })
+
+     Row (
+        modifier = Modifier
+           .fillMaxWidth()
+           .padding(40.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+     ){
+        Image(painter = painterResource(id = R.drawable.google),
+           contentDescription = "Google",
+           modifier = Modifier
+              .size(35.dp)
+              .clickable { })
+
+        Image(painter = painterResource(id = R.drawable.facebook),
+           contentDescription = "Facebook",
+           modifier = Modifier
+              .size(35.dp)
+              .clickable { })
+
+        Image(painter = painterResource(id = R.drawable.x),
+           contentDescription ="X",
+           modifier = Modifier
+              .size(60.dp)
+              .clickable { })
+     }
+
+
    }
 
 }
